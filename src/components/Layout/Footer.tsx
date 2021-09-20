4c172e283b68cb008c80a1dad15a5a6ffcb3c9ea
Joao Neiva <jneiva42@gmail.com>
2021-09-20 10:07:39 -0300

    create Footer with contact info

---
+++ src/components/Layout/Footer.tsx
@@ -0,0 +1,22 @@
+import { Text, HStack, IconButton, VStack, Link } from '@chakra-ui/react'
+import React from 'react'
+import { FaGithub, FaInstagram, FaLinkedin } from 'react-icons/fa'
+
+export const Footer = () => {
+  return (
+    <VStack mt={8} mb={2}>
+      <HStack>
+        <Link href='https://github.com/jneiva0/' isExternal>
+          <IconButton aria-label='GitHub' icon={<FaGithub />} />
+        </Link>
+        <Link href='https://www.linkedin.com/in/jneiva0/' isExternal>
+          <IconButton aria-label='LinkedIn' icon={<FaLinkedin />} />
+        </Link>
+        <Link href='https://instagram.com/jv.neiva/' isExternal>
+          <IconButton aria-label='Instagram' icon={<FaInstagram />} />
+        </Link>
+      </HStack>
+      <Text fontSize='sm'>© 2021 João Vitor Neiva</Text>
+    </VStack>
+  )
+}
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,6 +2,7 @@ import { Box } from '@chakra-ui/react'
 import { Header } from 'components/Layout/Header'
 import { MAX_WIDTH } from 'configs'
 import React, { ReactNode } from 'react'
+import { Footer } from './Footer'
 
 type Props = {
   children: ReactNode
@@ -14,7 +15,7 @@ export const Layout = ({ children }: Props) => {
       <Box maxW={MAX_WIDTH} px={[4, 6, 8, 12, 16]} mx='auto'>
         {children}
       </Box>
-      {/* <Footer /> */}
+      <Footer />
     </>
   )
 }
